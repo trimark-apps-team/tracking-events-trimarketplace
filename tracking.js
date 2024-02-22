d7e041bb6aaf5578ddd979539a7cc4e564cf39d2
Kindorf <kevin.kindorf@trimarkusa.com>
2024-02-22 12:21:16 -0500

    add commit for versioning

--- tracking.js
+++ tracking.js
@@ -2,6 +2,7 @@ window.addEventListener("load", (event) => {
     const dataLayer = window.dataLayer || [];
     console.log('hello')
     console.log('hello again')
+    console.log('hello a third time')
     console.log(dataLayer)
 
     dataLayer.push({
