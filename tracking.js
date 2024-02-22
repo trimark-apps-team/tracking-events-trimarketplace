94229cc3a0873f95f88e05e5e1570e471a29847b
Kindorf <kevin.kindorf@trimarkusa.com>
2024-02-22 11:00:01 -0500

    check if datalayer exists before doing anything

--- tracking.js
+++ tracking.js
@@ -1,8 +1,13 @@
-console.log('hello')
-console.log(dataLayer)
+window.addEventListener("load", (event) => {
+    const dataLayer = window.dataLayer || [];
+    console.log('hello')
+    console.log(dataLayer)
 
-dataLayer.push({
-    event: 'site_event',
-    event_category: 'test',
-    event_action: 'visit page'
+    dataLayer.push({
+        event: 'site_event',
+        event_category: 'test',
+        event_action: 'visit page'
+    });
 });
+
+
