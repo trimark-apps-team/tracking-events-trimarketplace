d8a9e13116d14a69b594418b0c69ad8bf3beb6b4
Kindorf <kevin.kindorf@trimarkusa.com>
2024-02-27 13:31:58 -0500

    add cookie functions

--- tracking.js
+++ tracking.js
@@ -6,7 +6,7 @@ window.addEventListener("load", (event) => {
 
 // session start event
 $(function () {
-    var sessionStarted = $.cookie("session_started");
+    var sessionStarted = getCookie("session_started");
     if(sessionStarted) {
         return
     }
@@ -17,9 +17,33 @@ $(function () {
         dataLayer.push({
             event: 'first_visit'
         })
-        $.cookie('session_started', true);
+        setCookie('session_started', true);
     }
   });
+
+
+
+// get and set cookie functions
+
+// session cookie is set
+  function setCookie(cname, cvalue) {
+    document.cookie = cname + "=" + cvalue + ";" + ";path=/";
+  }
+  
+  function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
 
 
 // 
