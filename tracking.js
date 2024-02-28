ccd7cfd57ce4fc304a3232ca46c654a705aca2af
Kindorf <kevin.kindorf@trimarkusa.com>
2024-02-28 12:20:39 -0500

    add more events

--- tracking.js
+++ tracking.js
@@ -3,15 +3,21 @@ window.addEventListener("load", (event) => {
     console.log(dataLayer)
     dataLayer.push({ ecommerce: null });  // Clear the previous ecommerce object.
     const btn = document.querySelector("button");
+    const productDetail = document.querySelector(".product-detail")
+    const downLoadDocument = document.querySelector(".download-document")
 
 // session start event
     var sessionStarted = getCookie("session_started");
     if(!sessionStarted) {
         dataLayer.push({
-            event: 'session_start'
+            event: 'session_start',
+            eventCategory: 'user',
+            eventLabel: ''
         });
         dataLayer.push({
-            event: 'first_visit'
+            event: 'first_visit',
+            eventCategory: 'user',
+            eventLabel: ''
         })
         setCookie('session_started', true);
     }
@@ -21,22 +27,82 @@ window.addEventListener("load", (event) => {
     var loggedInClass = document.querySelector('.user-info-logged-in')
     if(loggedInClass && !loggedInCookie) {
         dataLayer.push({
-            event: 'login'
+            event: 'login',
+            eventCategory: 'form',
+            eventLabel: ''
         });
         setCookie('logged_in', true)
-
     }
 
 // add to cart click event
-    btn.addEventListener("click", function () {
+    $("button").click(function() {
         console.log(this.innerHTML)
         if(this.innerHTML === 'ADD TO CART') {
             console.log('add to cart button clicked')
             dataLayer.push({
-                event: 'add_to_cart'
+                event: 'add_to_cart',
+                eventCategory: 'click'
             });
         }
-    });
+    })
+
+
+// view cart
+    if (window.location.href.indexOf("shopping-cart") > -1) {
+        dataLayer.push({
+            event: 'view_cart',
+            eventCategory: 'page view',
+            eventLabel: ''
+        });
+    }
+
+    // view item
+    if(productDetail && window.location.href.indexOf("product-details") > -1) {
+        const itemNumber = document.querySelector(".item-number-top span").textContent;
+        const productTitle = document.querySelector(".product-title h1");
+        dataLayer.push({
+            event: 'view_item',
+            eventCategory: 'page view',
+            eventLabel: productTitle
+            
+        });
+    }
+
+    // begin checkout event
+    const shippingStepLink = document.querySelector(".shipping-step a")
+    if(shippingStepLink.classList.contains('active')) {
+        dataLayer.push({
+            event: 'begin_checkout',
+            eventCategory: 'page view',
+            eventLabel: ''
+        });
+    }
+
+
+    // leaves website
+    window.addEventListener('beforeunload', function(e) {
+        const lastPageVisited = window.location.href
+        dataLayer.push({
+            event: 'outbound_click',
+            eventCategory: 'click',
+            eventLabel: lastPageVisited
+        })
+      });
+
+      $(".download-document").click(function() {
+        console.log('download button clicked')
+        const pageTitle = document.querySelector(".page-title")
+        dataLayer.push({
+            event: 'file_download',
+            eventCategory: 'click',
+            eventLabel: pageTitle
+        })
+      })
+
+      $(".delete-orderline").click(function() {
+        console.log('remove item clicked')
+      })
+
 
 });
 
